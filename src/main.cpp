c24f5ed037c6edce0ad2869eb3e44d0481041c0d
Arnaud Bletterer <arnaud.bletterer@gmail.com>
2017-07-03 08:38:25 +0200

    Ajout QGLViewer dans le projet comme bibliothèque externe

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,17 @@
 
 int main(int argc, char **argv) {
   // Read command lines arguments.
-std::cout << "lol" << std::endl;
-std::cout << "lol after" << std::endl;
-//  QApplication application(argc, argv);
+  QApplication application(argc, argv);
 
-//  // Instantiate the viewer.
-//  Viewer viewer;
+  // Instantiate the viewer.
+  Viewer viewer;
 
-//  viewer.setWindowTitle("bestViewer");
+  viewer.setWindowTitle("bestViewer");
 
-//  // Make the viewer window visible on screen.
-//  viewer.show();
+  // Make the viewer window visible on screen.
+  viewer.show();
 
-//  // Run main loop.
-//  return application.exec();
+  // Run main loop.
+  return application.exec();
 }
 
