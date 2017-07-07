d670d85e0cd4e4bb9c3241a35d6b011cac056c45
zinegit <tom.mourot-faraut@mines-nancy.org>
2017-07-07 09:24:45 +0200

    update de getCenter

---
+++ include/getCenter.hpp
@@ -0,0 +1,6 @@
+
+#include <vector>
+
+using namespace std;
+
+vector<float> getCenters(vector<float>& vertex_positions, vector<int>& index_triangles);
--- src/simpleViewer.cpp
+++ src/simpleViewer.cpp
@@ -312,32 +312,14 @@ void Viewer::draw()
 
 
 //        // Draw normals
-//        vector<float> barycentres_triangles;
+//        aezfzo = getCenters(zfzf);
 
-//        for (int i = 0; i < m_index.size(); i += 3)
+//        for (auto v : m_normals)
 //        {
-//            vector<float> triangle(m_vertex_positions);
-//            barycentres_triangles.push_back();
-//                a[0] = vertex_positions[3 * index_triangles[i+1]] - vertex_positions[3 * index_triangles[i]];
-//                a[1] = vertex_positions[3 * index_triangles[i+1]+1] - vertex_positions[3 * index_triangles[i]+1];
-//                a[2] = vertex_positions[3 * index_triangles[i+1]+2] - vertex_positions[3 * index_triangles[i]+2];
-//                b[0] = vertex_positions[3 * index_triangles[i+2]] - vertex_positions[3 * index_triangles[i]];
-//                b[1] = vertex_positions[3 * index_triangles[i+2]+1] - vertex_positions[3 * index_triangles[i]+1];
-//                b[2] = vertex_positions[3 * index_triangles[i+2]+2] - vertex_positions[3 * index_triangles[i]+2];
-
-//                Eigen::Vector3d a_e(a[0], a[1], a[2]);
-//                Eigen::Vector3d b_e(b[0], b[1], b[2]);
-//                n = a_e.cross(b_e);
-//                normals[i] = n[0];
-//                normals[i+1] = n[1];
-//                normals[i+2] = n[2];
+//            glBegin(GL_LINES);
+//                glVertex2f(10, 10);
+//            glEnd();
 //        }
-        for (auto v : m_normals)
-        {
-            glBegin(GL_LINES);
-                glVertex2f(10, 10);
-            glEnd();
-        }
     }
     else if (outside_camera)
     {
